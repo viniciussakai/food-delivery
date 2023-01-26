cea2f353350d42cb71fa67555092a1c26d1e887d
Vinicius Sakai <vinicius.sakai@protonmail.com>
2023-01-26 15:36:03 -0300

    project: Initialised git repository

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { registerRootComponent } from "expo";
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<Text>Hello World</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
+
+export default registerRootComponent(App);
