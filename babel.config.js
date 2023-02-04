6b51ae57858c4dfa8ceb3754cd3fdd71d5198726
Vinicius Sakai <vinicius.sakai@tuta.io>
2023-02-03 23:47:53 -0300

    project: Update path alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,38 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+
+		plugins: [
+			[
+				require.resolve("babel-plugin-module-resolver"),
+				{
+					extensions: [
+						".js",
+						".jsx",
+						".ts",
+						".tsx",
+						".android.js",
+						".android.tsx",
+						".ios.js",
+						".ios.tsx",
+					],
+					root: ["."],
+
+					alias: {
+						"@components": "./src/src/components",
+						"@screens": "./src/screens",
+						"@utils": "./src/utils",
+						"@hooks": "./src/hooks",
+						"@assets": "./src/assets",
+						"@types": "./src/types",
+						"@routes": "./src/routes",
+						"@config": "./src/config",
+						"@services": "./src/services",
+						"@styles": "./src/styles",
+					},
+				},
+			],
+		],
+	};
 };
